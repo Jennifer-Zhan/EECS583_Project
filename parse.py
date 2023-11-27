525a71883c2e4f5e7e2fb37837b2212d00ca3eda
pranavb <pranavb@umich.edu>
2023-11-27 15:26:56 -0500

    basic r/np marking on decls only

--- parse.py
+++ parse.py
@@ -16,12 +16,31 @@ class FuncDefVisitor(c_ast.NodeVisitor):
                 array_decl = c_ast.Decl(name=name, quals=[], storage=[], funcspec=[], type=array_type, bitsize=None, init=init_list, align=None)
                 
                 self.array_decls.append(array_decl)
+
+class LoopVisitor(c_ast.NodeVisitor):
+    def visit_For(self, node):
+        for i, stmt in enumerate(node.stmt.block_items):
+            if isinstance(stmt, c_ast.Decl) and isinstance(stmt.init, c_ast.ArrayRef) and stmt.init.name.name == 'A':
+                Aw_ref_check = c_ast.ArrayRef(name=c_ast.ID(name='Aw'), subscript=stmt.init.subscript)
+                condition = c_ast.BinaryOp(op='==', left=Aw_ref_check, right=c_ast.Constant(type='int', value='0'))
+                mark_Ar = c_ast.Assignment(op='=', lvalue = c_ast.ArrayRef(name=c_ast.ID(name='Ar'), subscript=stmt.init.subscript), rvalue=c_ast.Constant(type='int', value='1'))
+                mark_Anp = c_ast.Assignment(op='=', lvalue = c_ast.ArrayRef(name=c_ast.ID(name='Anp'), subscript=stmt.init.subscript), rvalue=c_ast.Constant(type='int', value='1'))
+                iftrue_stmts = c_ast.Compound(block_items=[mark_Ar, mark_Anp])
+                written_check = c_ast.If(cond=condition, iftrue=iftrue_stmts, iffalse=None)
+
+                node.stmt.block_items.insert(i + 1, written_check)
+            
+            # elif isinstance(stmt, c_ast.If):
+
+            
             
 def shadow_array(filename):
     ast = parse_file(filename)
 
     v = FuncDefVisitor()
     v.visit(ast)
+
+    # print(ast.ext)
 
     for i, decl in enumerate(ast.ext[0].body.block_items):
         if decl.name == 'A':
@@ -30,12 +49,13 @@ def shadow_array(filename):
     
     ast.ext[0].body.block_items = ast.ext[0].body.block_items[0:stop+1] + v.array_decls + ast.ext[0].body.block_items[stop+1:]
 
-    # Generate the modified C code
+    loop = LoopVisitor()
+    loop.visit(ast)
+
     generator = c_generator.CGenerator()
     modified_code = generator.visit(ast)
+
     print(modified_code)
 
-
 filename = 'example.c'
-
 shadow_array(filename)

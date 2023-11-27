3ce31e0be09b9fc99cff8b52af3f3ba4b31d926e
pranavb <pranavb@umich.edu>
2023-11-27 16:19:35 -0500

    checking inside if statements

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 from pycparser import c_ast, parse_file, c_generator
+import copy
 
 class FuncDefVisitor(c_ast.NodeVisitor):
     def __init__(self):
@@ -19,21 +20,46 @@ class FuncDefVisitor(c_ast.NodeVisitor):
 
 class LoopVisitor(c_ast.NodeVisitor):
     def visit_For(self, node):
+        # print(node.stmt.block_items)
+        updated_node = copy.deepcopy(node.stmt.block_items)
+
         for i, stmt in enumerate(node.stmt.block_items):
             if isinstance(stmt, c_ast.Decl) and isinstance(stmt.init, c_ast.ArrayRef) and stmt.init.name.name == 'A':
-                Aw_ref_check = c_ast.ArrayRef(name=c_ast.ID(name='Aw'), subscript=stmt.init.subscript)
-                condition = c_ast.BinaryOp(op='==', left=Aw_ref_check, right=c_ast.Constant(type='int', value='0'))
-                mark_Ar = c_ast.Assignment(op='=', lvalue = c_ast.ArrayRef(name=c_ast.ID(name='Ar'), subscript=stmt.init.subscript), rvalue=c_ast.Constant(type='int', value='1'))
-                mark_Anp = c_ast.Assignment(op='=', lvalue = c_ast.ArrayRef(name=c_ast.ID(name='Anp'), subscript=stmt.init.subscript), rvalue=c_ast.Constant(type='int', value='1'))
-                iftrue_stmts = c_ast.Compound(block_items=[mark_Ar, mark_Anp])
-                written_check = c_ast.If(cond=condition, iftrue=iftrue_stmts, iffalse=None)
+                updated_node.insert(i + 1, mark_read_and_priv(stmt.init.subscript))
+            
+            elif isinstance(stmt, c_ast.Assignment):
+                if isinstance(stmt.rvalue, c_ast.BinaryOp):
+                    subscr = None
+                    if isinstance(stmt.rvalue.left, c_ast.ArrayRef) and stmt.rvalue.left.name.name == 'A':
+                        subscr = stmt.rvalue.left.subscript
+                    elif isinstance(stmt.rvalue.right, c_ast.ArrayRef) and stmt.rvalue.right.name.name == 'A':
+                        subscr = stmt.rvalue.right.subscript
+                    
+                    if subscr is not None:
+                        updated_node.insert(i + 1, mark_read_and_priv(stmt.init.subscript))
+            
+            elif isinstance(stmt, c_ast.If):
+                if isinstance(stmt.cond, c_ast.BinaryOp):
+                    subscr = None
+                    if isinstance(stmt.cond.left, c_ast.ArrayRef) and stmt.cond.left.name.name == 'A':
+                        subscr = stmt.left.subscript
+                    if isinstance(stmt.cond.right, c_ast.ArrayRef) and stmt.cond.right.name.name == 'A':
+                        subscr = stmt.right.subscript
+                                
+                for j, ops in enumerate(stmt.iftrue.block_items):
+                    print(ops)
 
-                node.stmt.block_items.insert(i + 1, written_check)
-            
-            # elif isinstance(stmt, c_ast.If):
+def mark_read_and_priv(subscr):
+    Aw_ref_check = c_ast.ArrayRef(name=c_ast.ID(name='Aw'), subscript=subscr)
+    condition = c_ast.BinaryOp(op='==', left=Aw_ref_check, right=c_ast.Constant(type='int', value='0'))
+    mark_Ar = c_ast.Assignment(op='=', lvalue = c_ast.ArrayRef(name=c_ast.ID(name='Ar'), subscript=subscr), rvalue=c_ast.Constant(type='int', value='1'))
+    mark_Anp = c_ast.Assignment(op='=', lvalue = c_ast.ArrayRef(name=c_ast.ID(name='Anp'), subscript=subscr), rvalue=c_ast.Constant(type='int', value='1'))
+    iftrue_stmts = c_ast.Compound(block_items=[mark_Ar, mark_Anp])
+    written_check = c_ast.If(cond=condition, iftrue=iftrue_stmts, iffalse=None)
 
-            
-            
+    return written_check
+
+                    
 def shadow_array(filename):
     ast = parse_file(filename)
 
@@ -55,7 +81,7 @@ def shadow_array(filename):
     generator = c_generator.CGenerator()
     modified_code = generator.visit(ast)
 
-    print(modified_code)
+    # print(modified_code)
 
 filename = 'example.c'
 shadow_array(filename)

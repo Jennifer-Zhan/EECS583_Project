4040146e603c46bddf5265e826d5b3b6fe8f0b70
Zhan, Jennifer <ruoyizhan@gmail.com>
2023-12-13 14:42:45 -0500

    edit convert_parallel_openmp.py

--- src/convert_parallel_openmp.py
+++ src/convert_parallel_openmp.py
@@ -21,36 +21,31 @@ def convert_serial_to_parallel(filename, print_line):
     braces_num = 0
     start = False
     print_num = 0
+    open_brackets = 0
     output_file.write("#include <omp.h>\n")
     for line in input_file:
         current_line_num += 1
         number_space = len(line) - len(line.lstrip())
         add_critical = False
         if(line.strip()[:3] == "for" and braces_num == 0):
-            #print(str(current_line_num)+" "+str(print_line[print_num]))
             if(len(print_line) == 0 or current_line_num != print_line[print_num]):
                 output_file.write(number_space * " " + "#pragma omp parallel for\n")
                 start = True
-            #else:
-                #print_num += 1
-                #print(str(print_num)+" "+str(current_line_num))
         elif(line.strip()[:5] == "print"):
             print_num += 1
-            # print(str(print_num)+" "+str(current_line_num))
         if(start):
-            # add lock when there is +=
-            # print(line.strip().split())
             if "+=" in line.strip().split():
                 output_file.write(number_space * " " + "#pragma omp critical\n" + number_space * " " + "{\n")
                 add_critical = True
             if(line.strip()[-1] == "{"):
                 braces_num += 1
+                open_brackets += 1
             if(line.strip()[-1] == "}"):
                 braces_num -= 1
         output_file.write(line)
         if(add_critical):
             output_file.write(number_space * " " + "}\n")
-        if(start and braces_num == 0):
+        if(start and braces_num == 0 and not open_brackets == 0):
             output_file.write(number_space * " " + "#pragma omp barrier\n")
             start = False
 

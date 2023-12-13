1c1cf89490d53b85689653c87d342ba989f681e1
zyuhaoME <zyuhao@umich.edu>
2023-12-13 23:29:49 +0800

    correct 30000 simple example

---
+++ marked_examples/marked_simple_30000_iter_1.c
@@ -0,0 +1,53 @@
+int main() {
+  int A[30000] = {0};
+  int Aw[30000] = {0};
+
+  int Ar[30000] = {0};
+  int Anp[30000] = {0};
+  int Anx[30000] = {0};
+  int write_counter = 0;
+  int distinct_write_counter = 0;
+  int i;
+  for (i = 0; i < 30000; i++) {
+    int Awi[30000] = {0};
+    A[i] = A[i] + 5;
+    if (Awi[i] == 0) {
+      Ar[i] = 1;
+      Anp[i] = 1;
+    }
+    Aw[i] = 1;
+    Awi[i] = 1;
+    Ar[i] = 0;
+    write_counter += 1;
+  }
+
+  for (int i = 0; i < 30000; ++i) {
+    if (Aw[i] == 1) ++distinct_write_counter;
+  }
+
+  for (int i = 0; i < 30000; i++) {
+    printf("%d ", Aw[i]);
+  }
+  printf("\n");
+
+  for (int i = 0; i < 30000; i++) {
+    printf("%d ", Ar[i]);
+  }
+  printf("\n");
+
+  for (int i = 0; i < 30000; i++) {
+    printf("%d ", Anx[i]);
+  }
+  printf("\n");
+
+  for (int i = 0; i < 20000; i++) {
+    printf("%d ", Anp[i]);
+  }
+  printf("\n");
+
+  printf("%d ", write_counter);
+  printf("\n");
+  printf("%d ", distinct_write_counter);
+
+  return 0;
+}
